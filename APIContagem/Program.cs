f7ca5e02db1ba5e6f3cbc1f5a5a2058d39c1278c
Renato Groffe <renato.groffe@yahoo.com.br>
2020-10-24 14:34:04 -0300

    ASP.NET Core 3.1 + REST API + Contagem de Acessos + JWT + Azure App Configuration + Azure Key Vault
    ASP.NET Core 3.1 + REST API + Contagem de Acessos + JWT + Azure App Configuration + Azure Key Vault

---
+++ APIContagem/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace APIContagem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var settings = config.Build();
+                    config.AddAzureKeyVault(
+                        settings["AzureKeyVault:DNS"],
+                        settings["AzureKeyVault:ClientId"],
+                        settings["AzureKeyVault:ClientSecret"]);
+
+                    settings = config.Build();
+                    config.AddAzureAppConfiguration(options =>
+                    {
+                        options.Connect(settings["ConnectionStrings:AppConfiguration"])
+                            .ConfigureRefresh(refresh =>
+                            {
+                                refresh.Register("Saudacao");
+                            });
+                    });
+                })     
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
